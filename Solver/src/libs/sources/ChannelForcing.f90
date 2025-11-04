b7ed2dd7696ff7777ccba63241ee1404e42946f4
Oscar Marino <oscar.marino@upm.es>
2025-11-04 18:33:32 +0100

    Fix possible race condition in ChannelForcing

--- Solver/src/libs/sources/ChannelForcing.f90
+++ Solver/src/libs/sources/ChannelForcing.f90
@@ -29,8 +29,7 @@ public initializeChannel, updateChannel, channelSource
       real(kind=RP), parameter :: alpha    =  0.9_RP
 
 
-!!$acc declare create(f_x, f_y, f_z, dpdx, dpdy, dpdz, dp_part, flowDir, alpha)
-!$acc declare create(f_x, f_y, f_z, dpdx, dpdy, dpdz)
+!$acc declare create(f_x, f_y, f_z)
 
 !  ========
 contains
@@ -79,11 +78,6 @@ contains
 
        channelIsActive = .true.
 
-!!$acc update device(alpha)
-!!$acc update device(flowDir)
-!$acc update device(dpdx)
-!$acc update device(dpdy)
-!$acc update device(dpdz)
 !$acc update device(f_x)
 !$acc update device(f_y)
 !$acc update device(f_z)
@@ -113,10 +107,6 @@ contains
         !-------------------------------------------------------------
 
         if (.not. channelIsActive) return
-
-!$acc update self(dpdx)
-!$acc update self(dpdy)
-!$acc update self(dpdz)
 !
 !       Get the same global dpdx,dpdy,dpdz in all partitions
 !       ----------------------------------------------------
@@ -140,9 +130,6 @@ contains
            dp_ind  = maxloc(abs(dp_part),1)
            dpdz    = dp_part(dp_ind)
 #endif
-!$acc update device(dpdx)
-!$acc update device(dpdy)
-!$acc update device(dpdz)
 !
         end if
 !
@@ -174,45 +161,59 @@ contains
          Implicit None
          type(HexMesh), intent(in)         :: mesh
 
-         real(kind=RP) :: dp_vals(2), rho
-         integer       :: dp_ind, i, j, k, eID
+         real(kind=RP) :: rho, rho_max, rho_min, S_max, S_min
+         integer       :: i, j, k, eID, dp_ind
          real(kind=RP) :: Su,Sv,Sw,Se
+         real(kind=RP) :: dp_vals(3)
 
         if (.not. channelIsActive) return
+!
+        rho_min = huge(0.0)
+        rho_max = -huge(0.0)
 
-!$acc parallel loop gang vector_length(128) present(mesh) async(1) private(dp_vals)
+!$acc parallel loop gang vector_length(128) present(mesh) async(1)
          do eID = 1, mesh % no_of_elements
-            !$acc loop vector collapse(3) reduction(max:dpdx,dpdy,dpdz)
+            !$acc loop vector collapse(3) reduction(max:rho_max) reduction(min:rho_min)
             do k = 0, mesh % elements(eID) % Nxyz(3) ; do j = 0, mesh % elements(eID) % Nxyz(2) ; do i = 0, mesh % elements(eID) % Nxyz(1)
 
                 rho = mesh % elements(eID) % storage % Q(IRHO,i,j,k)
                 Su = f_x(1) * rho + f_x(2)
                 Sv = 0.0_RP
                 Sw = f_z(1) * rho + f_z(2)
-                Se = ( mesh % elements(eID) % storage % Q(IRHOU,i,j,k) / rho ) * Su + (mesh % elements(eID) % storage % Q(IRHOV,i,j,k) / rho ) * Sv + &
-                     ( mesh % elements(eID) % storage % Q(IRHOW,i,j,k) / rho ) * Sw
+                Se = ( mesh % elements(eID) % storage % Q(IRHOU,i,j,k) * Su + mesh % elements(eID) % storage % Q(IRHOV,i,j,k)* Sv + &
+                       mesh % elements(eID) % storage % Q(IRHOW,i,j,k) * Sw ) / rho 
 
                 mesh % elements(eID) % storage % S_NS(IRHOU,i,j,k) = Su
                 mesh % elements(eID) % storage % S_NS(IRHOV,i,j,k) = Sv
                 mesh % elements(eID) % storage % S_NS(IRHOW,i,j,k) = Sw
                 mesh % elements(eID) % storage % S_NS(IRHOE,i,j,k) = Se
-                
-                dp_vals = [dpdx,Su]
-                dp_ind  = maxloc(abs(dp_vals),1)
-                dpdx    = dp_vals(dp_ind)
-                
-                dp_vals = [dpdy,Sv]
-                dp_ind  = maxloc(abs(dp_vals),1)
-                dpdy    = dp_vals(dp_ind)
-                
-                dp_vals = [dpdz,Sw]
-                dp_ind  = maxloc(abs(dp_vals),1)
-                dpdz    = dp_vals(dp_ind)
 
+                rho_min = min(rho_min, rho)
+                rho_max = max(rho_max, rho)
+                
             end do                  ; end do                ; end do
          end do
 !$acc end parallel loop
+!
+! get the maximum absolute value using the extreme values of rho (only variable changing in the loop)
+        S_min   = f_x(1) * rho_min + f_x(2)
+        S_max   = f_x(1) * rho_max + f_x(2)
+        dp_vals = [dpdx,S_max,S_min]
+        dp_ind  = maxloc(abs(dp_vals),1)
+        dpdx    = dp_vals(dp_ind)
 
+        S_min   = f_y(1) * rho_min + f_y(2)
+        S_max   = f_y(1) * rho_max + f_y(2)
+        dp_vals = [dpdy,S_max,S_min]
+        dp_ind  = maxloc(abs(dp_vals),1)
+        dpdy    = dp_vals(dp_ind)
+
+        S_min   = f_z(1) * rho_min + f_z(2)
+        S_max   = f_z(1) * rho_max + f_z(2)
+        dp_vals = [dpdz,S_max,S_min]
+        dp_ind  = maxloc(abs(dp_vals),1)
+        dpdz    = dp_vals(dp_ind)
+!
      End Subroutine channelSource
 
 #endif
